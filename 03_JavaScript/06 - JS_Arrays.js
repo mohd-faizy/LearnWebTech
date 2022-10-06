d91cfae672565c07c2e6cee77b583b1c396fd74a
MOHD FAIZY <34925947+mohd-faizy@users.noreply.github.com>
2022-10-07 04:37:24 +0530

    update

--- 03_JavaScript/06 - JS_Arrays.js
+++ 03_JavaScript/06 - JS_Arrays.js
@@ -279,8 +279,8 @@ console.log(n)
 
 // ========================================================================
 // Que:5 Use JS `.reduce` method to calculate factorial of a given number
-//       from an array of first `n` natural numbers (`n` being the number whose
-//       factorial needs to be calculated)?
+//       from an array of first `n` natural numbers (`n` being the number 
+//       whose factorial needs to be calculated)?
 // ========================================================================
 
 let arr = [1, 2, 3, 4, 5]
