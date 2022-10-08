aa70fa3fa25c5a5a5713815987ac822d3f3ac931
MOHD FAIZY <34925947+mohd-faizy@users.noreply.github.com>
2022-10-09 02:28:55 +0530

    +Pro_Num_GuessGame

--- 03_JavaScript/JS_Projects/01P - JS_Game_Guessing_Number.js
+++ 03_JavaScript/JS_Projects/01P - JS_Game_Guessing_Number.js
@@ -1,13 +1,31 @@
-console.log("This is a simple game developed using javascript.\nYou will enter a range to generate a random number.\nThen computer will generate a random number within the range.\nYou have to guess the number.\n");
+// ================================Problem=======================================
+/* Write a JS Program to generate a Random Number and store it in a variable. the
+   Program then take an input from the user to them whether the guess was correct
+   , greater or lesser than the original number?*/
 
-let range = Number(prompt("Enter the upper limit of the number to generate"));
+/* 100 - (number of guesses) is  the score of the user, the program is expected to
+   terminate once the user guessed the number correctly) */
+
+/* The Number should be between 1 - 100*/ 
+// ==============================================================================
+// ===============================[Approch-1]====================================
+
+
+console.log("JS GAME: Guess the Correct Number Correctly.\n"); // Print this
+
+let range = Number.parseInt(prompt("Enter the upper limit of the number to generate"));
+
+// A random whole number between 1 and 100
 let num = (Math.random() * range) + 1;
 num = Math.floor(num);
+// settng up the Counter and number of chances
 let counter = 1;
-let chance = Math.floor(range / 2);
+let chance = Math.floor(range / 10);
 console.log(`You Have total ${chance} chances to guess the number`);
+
+// Main logic
 while (true) {
-  let guessed_num = Number(prompt("Guess the number generated"));
+  let guessed_num = Number.parseInt(prompt("Guess the number:"));
   if (num > guessed_num) {
     console.log("\nThe Number generated is greater");
     counter++;
@@ -26,3 +44,41 @@ while (true) {
     break;
   }
 }
+
+
+// ===============================[Approch-2]====================================
+let a = Math.random() * 100;
+a = Number.parseInt(a)
+let inp;
+let score = 100;
+
+while (inp != a) {
+  score = score - 1;
+  inp = prompt("Enter the Number : ");
+  if (inp == a) {
+    console.log("Congradulation! You guess the correct number.")
+    console.log(`You guess the actual number in ${100 - score} chances.`)
+  }
+  else if (inp > a && inp < 100) {
+    console.log("your number is greater than the actual number.")
+  }
+  else if (inp < a && inp > 0) {
+    console.log("your number is smaller than the actual number.")
+  }
+  else {
+    console.log("Enter number between 1 to 100")
+  }
+}
+
+
+
+
+
+// Random Number generation in JS:
+// --------------------------------------------------
+// Return a random number between 0 and 100: 
+console.log(Math.random() * 100);              // Sample Output: 16.19982094546135
+console.log(Math.floor(Math.random() * 100))   // Sample Output: 16
+// A random whole number between 1 and 100:
+console.log(Math.floor(Math.random() * 100) + 1);
+// --------------------------------------------------
